0b6ede2579b7628765e3d6595e974b760a527539
suryatejapadigela <suryatejapadigela491293@gmail.com>
2024-08-06 17:07:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function appendNumber(number) {
+    if (display.innerText === '0') {
+        display.innerText = number;
+    } else {
+        display.innerText += number;
+    }
+}
+
+function appendOperator(operator) {
+    display.innerText += operator;
+}
+
+function appendDot() {
+    if (!display.innerText.includes('.')) {
+        display.innerText += '.';
+    }
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
+
